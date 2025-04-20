d3c73d9c97ce8960d9c808dac7a4a6ceaf89a790
Lukas <MuhammedLukman2001@gmail.com>
2025-04-20 10:45:16 +0100

    improvement

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://eldglheibpfcfzpjfxbh.supabase.co";
-
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZGdsaGVpYnBmY2Z6cGpmeGJoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDIwMTQyNCwiZXhwIjoyMDU5Nzc3NDI0fQ.nqRliVAOuJu0QoR9V6Laik9_QJWQh5CuTtlWdQyiZ8c";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 export default supabase;
