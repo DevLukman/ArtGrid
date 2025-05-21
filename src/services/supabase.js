29d472a70bdd365be4e2a413a1e46f8e58a09dec
Lukas <MuhammedLukman2001@gmail.com>
2025-05-21 12:14:37 +0100

    clean up

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = " https://eldglheibpfcfzpjfxbh.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
