a000a453b39622f0a60d2e5089d42b54b0c6673d
Lukas <MuhammedLukman2001@gmail.com>
2025-05-23 22:19:09 +0100

    paystack

--- src/features/checkout/Paystack.jsx
+++ src/features/checkout/Paystack.jsx
@@ -3,7 +3,7 @@ import { PaystackButton } from "react-paystack";
 import { useCurrentUser } from "../authentication/useCurrentUser";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-const publicKey = import.meta.env.VITE_PAYSTACK_KEY;
+const publicKey = "pk_test_fefd5f0c57d317f14504c852b89e33ffdf0d920f";
 function Paystack({ amount }) {
   const navigate = useNavigate();
   const { userData = {} } = useCurrentUser();
